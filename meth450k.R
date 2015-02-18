911635efcbd753e31c4e1e06dc1882b298303fe9
Victor Wang <victoongo@gmail.com>
2015-02-18 14:53:19 -0500

    candidate gene results

--- meth450k.R
+++ meth450k.R
@@ -38,6 +38,21 @@ regr.site.pv <- foreach (y = bhv.vars, .verbose = TRUE, .packages = 'splines') %
 knitr::knit2pdf('~/Projects/R_lib/epigenetics/splines_table_site.Rnw')
 
 
+# top candidate genes by phenotype
+food <- c('AGRP','BRS3','CARTPT','CCKAR','CCKBR','FYN','GALR2','GALR3','GCG','GHRL',
+          'GHSR','HCRTR1','HCRTR2','HTR2C','LEP','MC4R','MCHR1','NMUR2','NPW','NPY',
+          'PMCH','PPYR1','PYY','UBR3','FTO')
+brain <- c('AFF2','ALK','ALX1','BPTF','CDK5R1','CEP290','CLN5','CNTN4','CTNS','DLX2',
+                 'DMBX1','DSCAML1','ECE2','EGR2','FOXG1','FOXP2','GLI2','GPR56','HESX1','LHX6',
+                 'MAP1S','MDGA1','MYO16','NCOA6','NDUFS4','NF1','NKX2-2','NNAT','OTX2','PBX1',
+                 'PBX3','PBX4','PCDH18','PHGDH','PITPNM1','POU6F1','PPT1','ROBO2','SHH','SHROOM2',
+                 'SHROOM4','SIX3','SLIT1','SMARCA1','TBR1','UBE3A','UNC5C','UTP3','VCX3A','ZIC1','ZIC2')
+candidate.genes <- list(food, brain)
+names(candidate.genes) <- c('food','brain')
+
+knitr::knit2pdf('~/Projects/R_lib/epigenetics/gene_specific.Rnw')
+
+
 
 ### mlm site
 #initialize the column we will use to store the methylation value one site at a time
